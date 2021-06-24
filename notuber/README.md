5768836e226a273167a6566401c5febd9cab0a84
Tianyi Zhang <tianyi.zhang0220@gmail.com>
2021-06-23 19:29:08 -0700

    Part 2 of notuber

--- notuber/README.md
+++ notuber/README.md
@@ -1,13 +1,6 @@
-Performance tests
-- Before performance optimization, it takes about 300-350ms for the webpage to load before any performance tests are done. The file 
-    sizes are 456B for index.html, 46B for style.css, and 954 for code.js
-- After perfomance optimization, the webpage takes about 290-340ms to load. The file sizes are 451B for index.html,
-    30B for style.css and 727B for code.js
-The load times of the normal webpage and then the performance optimization page are very close in values, but 
-the file sizes have a more significant difference as the sizes decrease by a noticeable amount.
-1. The google map and the location of each car was implemented correctly. The only error
-    is that when iterating through the jsonLocations, an error of unhandled promise rejection: type error is 
-    given so a try and catch block had to be made to avoid getting an error in the console. Map was also
-    not made to cover the entire screen so there would be some space for the header.
-2. I did not work with anyone on this lab.
-3. I spent about 4 hours on this lab, most of it was spent trying to figure out the google API.
+1. The lab has been implemented correctly, all the requirements were implemented. One going beyond 
+    requirment was implemented, the one with each car showing its distance from my location. There are 
+    typeErrors which are caught to avoid errors in the Javascript console. The Haversine formula from 
+    stackoverflow was implemented in the javascript portion of the lab.
+2. I did not collaborate with anyone on the lab
+3. This lab took about 4 hours.
--- notuber/code.js
+++ notuber/code.js
@@ -1,1 +1,132 @@
-function makeMap(){const n=new google.maps.Map(document.getElementById("map"),{zoom:14,center:{lat:42.352271,lng:-71.055242}});var o;for(jsonlocations='[{"id": "mXfkjrFw", "lat": 42.3453, "lng": -71.0464}, {"id": "nZXB8ZHz", "lat": 42.3662, "lng": -71.0621}, {"id": "Tkwu74WC", "lat": 42.3603, "lng": -71.0547}, {"id": "5KWpnAJN", "lat": 42.3472, "lng": -71.0802}, {"id": "uf5ZrXYw", "lat": 42.3663, "lng": -71.0544}, {"id": "VMerzMH8", "lat": 42.3542, "lng": -71.0704}]',obj=JSON.parse(jsonlocations),o=0;o<jsonlocations.length;o++)try{position={lat:obj[o].lat,lng:obj[o].lng},newMark=new google.maps.Marker({position:position,map:n}),newMark.setIcon("car.png")}catch(n){console.log("Unhandled Promise Rejection: TypeError")}}
+function makeMap() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+  
+function showPosition(position) {
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    const myPosition = { lat: lat, lng: long };
+    const map = new google.maps.Map(document.getElementById("map"), { zoom: 2, center: myPosition, });
+    myMark = new google.maps.Marker({ position: myPosition, map: map});
+
+    var http = new XMLHttpRequest();
+    var url = "https://jordan-marsh.herokuapp.com/rides";
+    var params = "username=vot48s6K&lat=" + lat + "&lng=" + long;
+
+    http.open('POST', url, true);
+    http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    list = http.send(params);
+    http.onreadystatechange = function(){
+        if(http.readyState == 4 && http.status == 200){
+            content = addCars(http.responseText, map, lat, long);
+            contentWindow = "<h1>Closest Car to my Position</h1>" + 
+                            "<p>ID: "+ content[1] + "</p>" +
+                            "<p>Username: " + content[2] + "</p>" +
+                            "<p>Distance: "+content[0]+" miles</p>";
+            const infoWindow = new google.maps.InfoWindow({content: contentWindow,});
+            myMark.addListener("click", ()=>{infoWindow.open({anchor: myMark, map, shouldFocus: false})});
+        }
+    }
+    
+    
+  }
+
+function addCars(list, map, lat, long) {
+    var i;
+    obj = JSON.parse(list);
+    for (i = 0; i<list.length; i++){
+        try{ 
+            addInfoWindow(obj, i, map, lat, long);
+            
+        } catch (TypeError){
+            console.log("Unhandled Promise Rejection: TypeError")
+        }
+    }
+    return findDistance(list, lat, long, map);
+  }
+
+function findDistance(list, lat, long, map) {
+    var i;
+    value = -1;
+    id = "";
+    username = "";
+    smallLat = 0;
+    smallLong = 0;
+    for (i = 0; i<list.length; i++){
+        try{
+            const R = 6371e3;
+            const one = lat * Math.PI/180;
+            const two = obj[i].lat * Math.PI/180;
+            const three = (obj[i].lat-lat) * Math.PI/180;
+            const four = (obj[i].lng-long) * Math.PI/180;
+
+            const a = Math.sin(three/2) * Math.sin(three/2) +
+                    Math.cos(one) * Math.cos(two) *
+                    Math.sin(four/2) * Math.sin(four/2);
+            const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+            const d = R * c;
+            if(value == -1 || d < value){
+                value = d;
+                id = obj[i].id;
+                username = obj[i].username;
+                smallLat = obj[i].lat;
+                smallLong = obj[i].lng;
+            }
+        } catch (TypeError){
+            console.log("Unhandled Promise Rejection: TypeError")
+        }
+        
+    }
+
+    const theLine = [{lat: lat, lng: long}, {lat: smallLat, lng: smallLong}];
+    const thePath = new google.maps.Polyline({
+        path: theLine,
+        geodesic: true,
+        strokeColor: "#8A2BE2",
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+    });
+    thePath.setMap(map);
+
+    value = value / 1609.344;
+    return [value, id, username];
+
+  }
+
+function addInfoWindow(obj, i, map, lat, long) {
+    position = { lat: obj[i].lat, lng: obj[i].lng, };
+    newMark = new google.maps.Marker({ position: position, map: map });
+    newMark.setIcon("car.png");
+    
+    const R = 6371e3; // metres
+    const one = lat * Math.PI/180; // φ, λ in radians
+    const two = obj[i].lat * Math.PI/180;
+    const three = (obj[i].lat-lat) * Math.PI/180;
+    const four = (obj[i].lng-long) * Math.PI/180;
+
+    const a = Math.sin(three/2) * Math.sin(three/2) +
+            Math.cos(one) * Math.cos(two) *
+            Math.sin(four/2) * Math.sin(four/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    const d = R * c / 1609.344;
+
+
+    contentWindow = "<h1>This car's info</h1>" + 
+                    "<p>ID: "+ obj[i].id + "</p>" +
+                    "<p>Username: " + obj[i].username + "</p>" + 
+                    "<p>Distance from me: " + d + " miles</p>";
+
+    var infoWindow = new google.maps.InfoWindow({content: contentWindow});
+    newMark.infowindow = infoWindow;
+    newMark.addListener("click", function() {
+        return this.infowindow.open(map, this);
+    });
+}
+
+
